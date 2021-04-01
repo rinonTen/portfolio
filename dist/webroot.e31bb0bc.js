121e16dd2cd67149b1d5ece0c28c57186eae19a8
Rinon TENDRINOMENA <rinon.ten@onja.org>
2021-04-01 18:07:33 +0300

    Changing the screenshot of the projets

--- dist/webroot.e31bb0bc.js
+++ dist/webroot.e31bb0bc.js
@@ -36469,7 +36469,7 @@ const worksData = [{
   "title": "Birthday app",
   "description": ["This app reminds the users of upcoming birthdays, so they never again have to apologize for forgetting it.", "I built the foundation of this website using HTML and CSS, while most of the visual components are generated using JavaScript. To retrieve and store data I used a mixture of API fetching and local storage.", "By doing this project, I improved my skills at using multiple filters in unison as well as manipulating complex date formats. I am especially proud of the way in which I resolved the issues that came up along the way, leading to a satisfying result."],
   "requirement": ["HTML", "CSS", "Vanilla JS"],
-  "screenshot": "./images/shotsnapp-birthday-app.png",
+  "screenshot": "./images/birthday-app.png",
   "url": "htpps://birthtday-app-rinon.netlify.app/",
   "codeUrl": "https://birthtday-app-rinon.netlify.app/"
 }, {
@@ -36477,7 +36477,7 @@ const worksData = [{
   "title": "Country Quiz",
   "description": ["This app allows users to play a quiz about countries' flags and capitals in a fun and interactive way", "In this project, I used React with global state management (useContext) which was necessary to simplify the code and keep it DRY. For styling, I used styled-components which is my favorite way of styling React components.", "I deepened my knowledge on the context API as well as randomising data. This project reinstated my confidence in my ability to find solutions even for problems that seem insurmountable at first."],
   "requirement": ["React", "styled-components", "CSS"],
-  "screenshot": "./images/shotsnapp-country-quiz.png",
+  "screenshot": "./images/country-quiz.png",
   "url": "https://country-quiz-rinon.netlify.app/",
   "codeUrl": "https://github.com/ganamavo/country-quiz"
 }, {
@@ -36485,7 +36485,7 @@ const worksData = [{
   "title": "Photograph",
   "description": ["This is one of my earlier projects that provides a porfolio that a photographer can use to present their work.", "I built this with SASS, HTML and JavaScript. Completing this project enhanced my skills at using CSS frameworks such as SASS as well as applying functionality to an image slider with JavaScript.", "This was one of the first times I used JavaScript in a project."],
   "requirement": ["HTML", "SASS", "JavaScript"],
-  "screenshot": "./images/shotsnapp-fotograph.png",
+  "screenshot": "./images/photograph.png",
   "url": "https://rinon-photograph-page.netlify.app/",
   "codeUrl": "https://github.com/ganamavo/photograf-page"
 } // {

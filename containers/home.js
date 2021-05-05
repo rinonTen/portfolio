5d6e09f20f3958b09330ea1e36030f98a5dd8bcb
Rinon <rinon.ten@onja.org>
2021-05-05 16:27:59 +0300

    Changing images format

--- containers/home.js
+++ containers/home.js
@@ -1,53 +1,52 @@
-import React, { useContext } from 'react';
-import { Context } from '../contexts/globalContext';
-import { Home, Works } from '../components';  
-import WorksContainer  from './works';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react'
+import { Context } from '../contexts/globalContext'
+import { Home, Works } from '../components'
+import WorksContainer from './works'
+import { Link } from 'react-router-dom'
 
-export default function HomeContainer() { 
-    const { works} = useContext(Context);
+export default function HomeContainer() {
+  const { works } = useContext(Context)
 
-    const worksElements = works && works.slice(0, 3).map(work => <WorksContainer key={work.id} {...work}/>)
+  const worksElements =
+    works &&
+    works.slice(0, 3).map((work) => <WorksContainer key={work.id} {...work} />)
 
-     return (
-        <Home>
-            <Home.SectionContainer>
-                 <Home.ImageContainer>
-                    <Home.Profile src="./images/my_image.JPG" alt="my image"/>
-                </Home.ImageContainer>
-                <Home.Article>
-                    <Home.Header>
-                        <Home.Heading>
-                            Hi, I am Rinon,
-                            Front-end developer
-                        </Home.Heading>
-                    </Home.Header>
-                    <Home.Text>
-                        I believe we can make the world a much better place through building products that solve people's problems. If you align with that vision, we should work together!
-                    </Home.Text> 
-                    <Home.ResumeButton>
-                        <Link to="/works">
-                            See projects
-                        </Link>
-                    </Home.ResumeButton>
-                </Home.Article>
-                <Home.ImageContainer>
-                    <Home.Profile src="./images/my_image.JPG" alt="my image"/>
-                    <Home.ImgShadow/>
-                </Home.ImageContainer>
-            </Home.SectionContainer> 
-            <Works>
-            <Works.Group>
-                <Works.Section>
-                    <Works.SectionContainer className="showOneWork">
-                        <Works.SectionHeader>
-                            <Works.Title>Featured works</Works.Title>
-                        </Works.SectionHeader>
-                        {worksElements}
-                    </Works.SectionContainer>
-                </Works.Section>
-            </Works.Group>
-        </Works> 
-        </Home>
-    )
+  return (
+    <Home>
+      <Home.SectionContainer>
+        <Home.ImageContainer>
+          <Home.Profile src='./images/my_image.JPG' alt='my image' />
+        </Home.ImageContainer>
+        <Home.Article>
+          <Home.Header>
+            <Home.Heading>Hi, I am Rinon, Front-end developer</Home.Heading>
+          </Home.Header>
+          <Home.Text>
+            I believe we can make the world a much better place through building
+            products that solve people's problems. If you align with that
+            vision, we should work together!
+          </Home.Text>
+          <Home.ResumeButton>
+            <Link to='/works'>See projects</Link>
+          </Home.ResumeButton>
+        </Home.Article>
+        <Home.ImageContainer>
+          <Home.Profile src='./images/my-image.webp' alt='my image' />
+          <Home.ImgShadow />
+        </Home.ImageContainer>
+      </Home.SectionContainer>
+      <Works>
+        <Works.Group>
+          <Works.Section>
+            <Works.SectionContainer className='showOneWork'>
+              <Works.SectionHeader>
+                <Works.Title>Featured works</Works.Title>
+              </Works.SectionHeader>
+              {worksElements}
+            </Works.SectionContainer>
+          </Works.Section>
+        </Works.Group>
+      </Works>
+    </Home>
+  )
 }
